b9f5f75a536b3418527695ffe62e157454eace7c
Kristian Hansson <kristian.hansson@helsinki.fi>
2014-03-31 17:36:28 +0300

    Scandinavian chars

---
+++ bibtex-for-booze/lib/scand_generator.rb
@@ -0,0 +1,5 @@
+module ScandGenerator
+    def generate_scands(strparam)
+        strparam.gsub(/[äöåÄÖÅ]/, 'ä' => "\"{a}", 'ö' => "\"{o}", 'å' => "\"aa", 'Ä' => "\"{A}", 'Ö' => "\"{O}", 'Å' => "\"AA")
+    end
+end
