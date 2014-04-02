8970e27a2575467e83fb9dce4d3f2ef15e79152a
Kristian Hansson <kristian.hansson@helsinki.fi>
2014-04-02 16:46:16 +0300

    Modulen tekoa

--- bibtex-for-booze/lib/bibtex_generator.rb
+++ bibtex-for-booze/lib/bibtex_generator.rb
@@ -1,6 +1,10 @@
 module BibtexGenerator
     
     def generate_bibtex
+        str = '@' + reference.referencetype + '{'
+        reference.attributes.each_pair do |name, value|
+            
+        end
         
     end
     
