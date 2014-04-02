db07c80dc0a8e918647a113abeaf42d1066f9040
Frans Ojala <frans.ojala@helsinki.fi>
2014-04-02 20:43:00 +0300

    view modifications

--- app/models/inproceeding.rb
+++ app/models/inproceeding.rb
@@ -1,4 +1,10 @@
-class Inproceeding < ActiveRecord::Base
+class Inproceeding < Reference
+
+validates :author, length: { minimum:3 }
+validates :title, length: {minimum: 3}
+validates :booktitle, length: {minimum: 3}
+validates :year, numericality: { greater_than_or_equal_to: 1800,
+                                 less_than_or_equal_to: Date.today.year }
 
 
 
