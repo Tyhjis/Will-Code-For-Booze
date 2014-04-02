38d7804df22430b8a5ab00aae0580f768f941a1d
Frans Ojala <frans.ojala@helsinki.fi>
2014-04-02 16:40:52 +0300

    added funtionality to references controller

--- bibtex-for-booze/app/controllers/references_controller.rb
+++ bibtex-for-booze/app/controllers/references_controller.rb
@@ -1,7 +1,19 @@
 class ReferencesController < ApplicationController
+  before_action :set_reference, only: :show
 
   def index
     @references = Reference.all
+  end
+
+  def show
+  end
+
+  def new
+    @reference = Reference.new
+  end
+
+  def set_reference
+    @reference = Reference.find(params[:id])
   end
 
 end
