bcd38f72cdf1ace63ff494a00a098b8c1621bcc2
Frans Ojala <frans.ojala@helsinki.fi>
2014-04-02 16:25:20 +0300

    resources for references

--- bibtex-for-booze/config/routes.rb
+++ bibtex-for-booze/config/routes.rb
@@ -1,6 +1,6 @@
 BibtexForBooze::Application.routes.draw do
 
-  resources :inproceedings
+  resources :references
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
