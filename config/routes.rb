96382337c20d21b874dca5b419325e5f1f2770bd
Frans Ojala <frans.ojala@helsinki.fi>
2014-04-02 20:05:16 +0300

    site root now configured at references#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ BibtexForBooze::Application.routes.draw do
 
   resources :references
 
+  root 'references#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
