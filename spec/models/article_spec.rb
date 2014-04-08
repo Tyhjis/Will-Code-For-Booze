d7dec97228f7a7df884ab1c2297a6471ee9c082d
Frans Ojala <frans.ojala@helsinki.fi>
2014-04-08 16:55:21 +0300

    fixed tests to take into account top level requirements

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,32 +5,38 @@ describe Article do
   #author title journal year
 
   it "is not saved if required field year not present" do
-    article = Article.create author:"Author", title:"Title", journal:"journal title"
+    article = Article.create author:"Author", title:"Title", journal:"journal title", referencetype:"article", key:"thekey"
     expect(article).not_to be_valid
   end
 
   it "is not saved if required field journal not present" do
-    article = Article.create author:"Author", title:"Title", year:1990
+    article = Article.create author:"Author", title:"Title", year:1990, referencetype:"article", key:"thekey"
     expect(article).not_to be_valid
   end
 
   it "is not saved if required field title not present" do
-    article = Article.create author:"Author", journal:"journal title", year:1990
+    article = Article.create author:"Author", journal:"journal title", year:1990, referencetype:"article", key:"thekey"
     expect(article).not_to be_valid
   end
 
   it "is not saved if required field author not present" do
-    article = Article.create title:"Title", journal:"journal title", year:1990
+    article = Article.create title:"Title", journal:"journal title", year:1990, referencetype:"article", key:"thekey"
+    expect(article).not_to be_valid
+  end
+
+  it "is not saved if required field referencetype not present" do
+    article = Article.create author:"Author", title:"Title", journal:"journal title", year:1990, key:"thekey"
     expect(article).not_to be_valid
   end
 
   it "is saved if all required fields present" do
-    article = Article.create author:"Author", title:"Title", journal:"journal title", year:1990
+    article = Article.create author:"Author", title:"Title", journal:"journal title", year:1990, referencetype:"article", key:"thekey"
     expect(article).to be_valid
   end
 
   it "is saved with all given fields" do
-    article = Article.create author:"Author", title:"Title", journal:"journal title", year:1990, publisher:"Jaska", note:"olen ihana"
+    article = Article.create author:"Author", title:"Title", journal:"journal title", year:1990, publisher:"Jaska",
+                             note:"olen ihana", referencetype:"article", key:"thekey"
     expect(article).to be_valid
 
     expect(article.note).to eq("olen ihana")
--- spec/models/inproceeding_spec.rb
+++ spec/models/inproceeding_spec.rb
@@ -3,32 +3,38 @@ require 'spec_helper'
 describe Inproceeding do
 
   it "is not saved if required field year not present" do
-    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle"
+    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle", referencetype:"inproceeding", key:"thekey"
     expect(inproceeding).not_to be_valid
   end
 
   it "is not saved if required field booktitle not present" do
-    inproceeding = Inproceeding.create author:"Author", title:"Title", year:1990
+    inproceeding = Inproceeding.create author:"Author", title:"Title", year:1990, referencetype:"inproceeding", key:"thekey"
     expect(inproceeding).not_to be_valid
   end
 
   it "is not saved if required field title not present" do
-    inproceeding = Inproceeding.create author:"Author", booktitle:"Booktitle", year:1990
+    inproceeding = Inproceeding.create author:"Author", booktitle:"Booktitle", year:1990, referencetype:"inproceeding", key:"thekey"
     expect(inproceeding).not_to be_valid
   end
 
   it "is not saved if required field author not present" do
-    inproceeding = Inproceeding.create title:"Title", booktitle:"Booktitle", year:1990
+    inproceeding = Inproceeding.create title:"Title", booktitle:"Booktitle", year:1990, referencetype:"inproceeding", key:"thekey"
+    expect(inproceeding).not_to be_valid
+  end
+
+  it "is not saved if required field referencetype not present" do
+    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle", year:1990, key:"thekey"
     expect(inproceeding).not_to be_valid
   end
 
   it "is saved if all required fields present" do
-    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle", year:1990
+    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle", year:1990, referencetype:"inproceeding", key:"thekey"
     expect(inproceeding).to be_valid
   end
 
   it "is saved with all given fields" do
-    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle", year:1990, publisher:"Jaska", note:"olen ihana"
+    inproceeding = Inproceeding.create author:"Author", title:"Title", booktitle:"Booktitle", year:1990, publisher:"Jaska",
+                                       note:"olen ihana", referencetype:"inproceeding", key:"thekey"
     expect(inproceeding).to be_valid
 
     expect(inproceeding.note).to eq("olen ihana")
