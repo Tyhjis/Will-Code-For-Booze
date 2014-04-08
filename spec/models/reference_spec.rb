bb5a7895447d3753644bf6d1a2bde9155f5eb704
Kristian Hansson <kristian.hansson@helsinki.fi>
2014-04-08 14:15:54 +0300

    referenssiä korjattu

--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -9,9 +9,19 @@ describe Reference do
   end
 
   it "returns correct bibtex string" do
-        ref = Reference.new author:'First Author and Second Author', title:'Some publication title', booktitle:'lol', year:1653
-        str = ref.to_inproceedings_bib_str
-        str.should == '@INPROCEEDINGS{ author = {First Author and Second Author}, title = {Some publication title}, booktitle = {lol}, year = {1653}}'
+        ref = Reference.new key:'erkki', referencetype:'inproceedings', author:'First Author and Second Author', title:'Some publication title', booktitle:'lol', year:1653
+        str = ref.to_bib_str
+        str.should == '@INPROCEEDINGS{ author = {First Author and Second Author}, title = {Some publication title}, booktitle = {lol}, year = {1653}, key = {erkki}}'
+  end
+
+  it "validates the required field key" do
+        ref = Reference.create referencetype:'inproceedings', author:'Erkki Hervanta', title:'Boozeman journal'
+        expect(ref.valid?).to be(false)
+  end
+
+  it "validates the required field referencetype" do
+        ref = Reference.create key:'erkki', author:'jykä käteinen'
+        expect(ref.valid?).to be(false)
   end
 end
 
