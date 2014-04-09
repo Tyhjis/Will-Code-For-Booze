548a37d798cfcf5543b249ebe4e588220703a1c2
Ari Vuolas <ari.vuolas@cs.helsinki.fi>
2014-04-09 18:08:05 +0300

    inproceedings key parrametrin hakua korjattu

--- app/controllers/inproceedings_controller.rb
+++ app/controllers/inproceedings_controller.rb
@@ -23,7 +23,7 @@ class InproceedingsController < ApplicationController
 
   def create
     @inproceeding = Inproceeding.new params.require(:inproceeding).permit(:author, :title, :booktitle, :year,:editor,
-                                                                 :volume, :series, :pagestart, :pageend, :address, :month, :organization, :publisher, :note)
+                                                                 :volume, :series, :pagestart, :pageend, :address, :month, :organization, :publisher, :note, :key)
 
     @inproceeding.referencetype = 'Inproceedings'
     if @inproceeding.key.nil? or @inproceeding.key.empty?
