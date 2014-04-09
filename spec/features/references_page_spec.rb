895ccd742fcc9e27c4de321a769dd69f2fb30177
Frans Ojala <frans.ojala@helsinki.fi>
2014-04-09 17:17:07 +0300

    some reference feature tests

--- spec/features/references_page_spec.rb
+++ spec/features/references_page_spec.rb
@@ -2,6 +2,36 @@ require 'spec_helper'
 
 describe 'References page' do
 
-  
+  before :all do
+    self.use_transactional_fixtures = false
+    WebMock.disable_net_connect!(allow_localhost:true)
+  end
+
+  before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  after :all do
+    self.use_transactional_fixtures = true
+  end
+
+  it 'shows links to creating article, inproceedings and book reference' do
+
+    visit references_path
+    expect(page).to have_content "New Inproceeding"
+    expect(page).to have_content "New Article"
+    expect(page).to have_content "New Book"
+
+  end
+
+  it 'is definitely listing references' do
+    visit references_path
+    expect(page).to have_content "Listing references"
+  end
 
 end
