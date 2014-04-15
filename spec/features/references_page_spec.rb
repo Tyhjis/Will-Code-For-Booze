ab18c209990b61177d4591bd14bb8f66eb10afe5
Frans Ojala <frans.ojala@cs.helsinki.fi>
2014-04-15 13:48:02 +0300

    more reference feature and book tests

--- spec/features/references_page_spec.rb
+++ spec/features/references_page_spec.rb
@@ -29,9 +29,52 @@ describe 'References page' do
 
   end
 
+  it 'shows correct bibtex string in showing reference' do
+    visit references_path
+    click_link 'New Book'
+    fill_in('book_key', with:'1234569992')
+    fill_in('book_author', with:'Erkki Hervanta')
+    fill_in('book_title', with:'Boozeman journal')
+    fill_in('book_publisher', with:'Helsinki')
+    fill_in('book_year', with:'1999')
+    click_button('Create Book')
+
+    click_link 'Show'
+    expect(page).to have_content '@BOOK{1234569992, author = {Erkki Hervanta}, title = {Boozeman journal}, year = {1999}, publisher = {Helsinki}}'
+  end
+
+  it 'show correct bibtex string for all references' do
+    make_books
+
+    click_link 'Get BIBTEX'
+
+    expect(page).to have_content '@BOOK{1234569992, author = {Erkki Hervanta}, title = {Boozeman journal}, year = {1999}, publisher = {Helsinki}}@BOOK{123456999, author = {Erkki Hervanta}, title = {Boozeman journal}, year = {1999}, publisher = {Helsinki}}'
+  end
+
   it 'is definitely listing references' do
     visit references_path
     expect(page).to have_content 'Listing references'
   end
+
+end
+
+def make_books
+  visit references_path
+  click_link 'New Book'
+  fill_in('book_key', with:'1234569992')
+  fill_in('book_author', with:'Erkki Hervanta')
+  fill_in('book_title', with:'Boozeman journal')
+  fill_in('book_publisher', with:'Helsinki')
+  fill_in('book_year', with:'1999')
+  click_button('Create Book')
+
+  visit references_path
+  click_link 'New Book'
+  fill_in('book_key', with:'123456999')
+  fill_in('book_author', with:'Erkki Hervanta')
+  fill_in('book_title', with:'Boozeman journal')
+  fill_in('book_publisher', with:'Helsinki')
+  fill_in('book_year', with:'1999')
+  click_button('Create Book')
 
 end
