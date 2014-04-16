5d64c1506c0c55accdba36147f083677ba761532
Ari Vuolas <ari.vuolas@cs.helsinki.fi>
2014-04-16 14:14:36 +0300

    viitteiden poiston testaus

--- spec/features/references_page_spec.rb
+++ spec/features/references_page_spec.rb
@@ -70,7 +70,51 @@ describe 'References page' do
 
   end
 
+  it 'has a button for destroying references' do
+    make_book
+    visit references_path
+    expect(page).to have_content 'Destroy'
+  end
+
+  it 'has a button for destroying individual book and it works' do
+    make_book
+    visit references_path
+
+    expect{
+      page.first('a', text:'Destroy').click
+    }.to change{Reference.count}.by(-1)
+    expect(page).to have_content 'Reference was successfully destroyed'
+    expect(page).not_to have_content 'Erkki Hervanta'
+  end
+
+
+  it 'has a button for destroying individual inproceeding and it works' do
+    make_inproceeding
+    visit references_path
+
+    expect{
+      page.first('a', text:'Destroy').click
+    }.to change{Reference.count}.by(-1)
+    expect(page).to have_content 'Reference was successfully destroyed'
+    expect(page).not_to have_content 'Erkki Hervanta'
+  end
+
+
+  it 'has a button for destroying individual article and it works' do
+    make_article
+    visit references_path
+
+    expect{
+      page.first('a', text:'Destroy').click
+    }.to change{Reference.count}.by(-1)
+    expect(page).to have_content 'Reference was successfully destroyed'
+    expect(page).not_to have_content 'Erkki Hervanta'
+  end
+
+
 end
+
+
 
 def make_books
   visit references_path
@@ -90,5 +134,39 @@ def make_books
   fill_in('book_publisher', with:'Helsinki')
   fill_in('book_year', with:'1999')
   click_button('Create Book')
+
+end
+
+def make_book
+  visit references_path
+  click_link 'New Book'
+  fill_in('book_key', with:'1234569992')
+  fill_in('book_author', with:'Erkki Hervanta')
+  fill_in('book_title', with:'Boozeman journal')
+  fill_in('book_publisher', with:'Helsinki')
+  fill_in('book_year', with:'1999')
+  click_button('Create Book')
+
+end
+def make_inproceeding
+  visit references_path
+  click_link 'New Inproceeding'
+  fill_in('inproceeding_key', with:'1234569992')
+  fill_in('inproceeding_author', with:'Erkki Hervanta')
+  fill_in('inproceeding_title', with:'Boozeman journal')
+  fill_in('inproceeding_booktitle', with:'Helsinki')
+  fill_in('inproceeding_year', with:'1999')
+  click_button('Create Inproceeding')
+
+end
+def make_article
+  visit references_path
+  click_link 'New Article'
+  fill_in('article_key', with:'1234569992')
+  fill_in('article_author', with:'Erkki Hervanta')
+  fill_in('article_title', with:'Boozeman journal')
+  fill_in('article_journal', with:'Helsinki')
+  fill_in('article_year', with:'1999')
+  click_button('Create Article')
 
 end
