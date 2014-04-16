280b7ae89e3bf461bf0ca630543fec9ebe59cce9
Frans Ojala <frans.ojala@cs.helsinki.fi>
2014-04-16 13:43:14 +0300

    inproceeding edit test done

--- spec/features/inproceedings_page_spec.rb
+++ spec/features/inproceedings_page_spec.rb
@@ -10,7 +10,7 @@ describe 'Inproceedings page' do
   before :each do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.start
-    visit new_inproceeding_path
+    visit references_path
   end
 
   after :each do
@@ -22,11 +22,12 @@ describe 'Inproceedings page' do
   end
 
   it 'should have Author field' do
+    click_link 'New Inproceeding'
     expect(page).to have_content 'Author'
   end
 
   it 'shows all required fields in creation' do
-    visit new_inproceeding_path
+    click_link 'New Inproceeding'
     expect(page).to have_content 'Required fields:'
     expect(page).to have_content 'Author:'
     expect(page).to have_content 'Title:'
@@ -34,8 +35,44 @@ describe 'Inproceedings page' do
     expect(page).to have_content 'Year:'
   end
 
+  it 'correctly modifies an existing attribute' do
+    click_link 'New Inproceeding'
+    fill_in('inproceeding_author', with:'jörö')
+    fill_in('inproceeding_title', with:'teosX')
+    fill_in('inproceeding_booktitle', with:'jotain')
+    fill_in('inproceeding_year', with:'2012')
+    click_button('Create Inproceeding')
+
+    click_link 'Edit'
+    expect(page).to have_content 'Edit Inproceedings:'
+
+    fill_in('inproceeding_author', with:'Jaska Jokunen')
+    click_button 'Update Inproceeding'
+    expect(page).to have_content 'Inproceedings was successfully updated.'
+    expect(page).not_to have_content 'jörö'
+    expect(page).to have_content 'Jaska Jokunen'
+  end
+
+  it 'correctly includes a new attribute' do
+    click_link 'New Inproceeding'
+    fill_in('inproceeding_author', with:'jörö')
+    fill_in('inproceeding_title', with:'teosX')
+    fill_in('inproceeding_booktitle', with:'jotain')
+    fill_in('inproceeding_year', with:'2012')
+    click_button('Create Inproceeding')
+
+    click_link 'Edit'
+    expect(page).to have_content 'Edit Inproceedings:'
+
+    fill_in('inproceeding_editor', with:'Salamanteri')
+    click_button 'Update Inproceeding'
+    expect(page).to have_content 'Inproceedings was successfully updated.'
+    click_link 'Show'
+    expect(page).to have_content 'Editor: Salamanteri'
+  end
+
   it 'does not save an article with not all required fields present' do
-    visit new_inproceeding_path
+    click_link 'New Inproceeding'
     fill_in('inproceeding_author', with:'jaska jokunen')
     fill_in('inproceeding_title', with:'jokusen muistelma')
     click_button('Create Inproceeding')
@@ -45,6 +82,7 @@ describe 'Inproceedings page' do
   end
 
   it 'should create new inproceeding reference with miminum requirements' do
+    click_link 'New Inproceeding'
     fill_in('inproceeding_author', with:'jörö')
     fill_in('inproceeding_title', with:'teosX')
     fill_in('inproceeding_booktitle', with:'jotain')
